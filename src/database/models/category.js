157100390bfb34b5d74f53763bae2f43825c815c
Thayssa Bernardo <thayssabernardo@gmail.com>
2022-08-25 19:27:33 -0300

    Cria model Category

---
+++ src/database/models/category.js
@@ -0,0 +1,17 @@
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define("Category", {
+    id: { 
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    }, name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  );  
+  return Category;
+};
+    
+module.exports = Category;
