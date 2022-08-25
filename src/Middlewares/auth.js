9f15979c594910d32e09c0fd015fe37061a11967
Thayssa Bernardo <thayssabernardo@gmail.com>
2022-08-25 19:15:05 -0300

    Adiciona referencia validação token

--- src/Middlewares/auth.js
+++ src/Middlewares/auth.js
@@ -7,6 +7,7 @@ const validateAuth = (req, res, next) => {
     
     if (!token) return res.status(401).json({ message: 'Token not found' });
 
+    // Referencia https://www.luiztools.com.br/post/autenticacao-json-web-token-jwt-em-nodejs/
     jwt.verify(token, JWT_SECRET, (err) => {
         if (err) { 
             return res.status(401).json({ message: 'Expired or invalid token' });
