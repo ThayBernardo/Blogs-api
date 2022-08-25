ec482171d2423be9569db3e3e0831db250c7a448
Thayssa Bernardo <thayssabernardo@gmail.com>
2022-08-25 15:40:06 -0300

    Criação do model User

---
+++ src/database/models/user.js
@@ -0,0 +1,30 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: { 
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    displayName: { 
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: { 
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: { 
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: { 
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  
+  return User;
+};
+  
+module.exports = User;
